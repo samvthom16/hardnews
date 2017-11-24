dc4a2a0a8faf2e45c11b5a234b3514e014fc5ee0
Samuel Thomas <samvthom16@gmail.com>
2017-11-24 19:08:30 +0530

    Enable pagination in grid3

--- pq-templates/articles-grid3.php
+++ pq-templates/articles-grid3.php
@@ -1,7 +1,9 @@
 <div class="container">
 	<section class="col-md-12 clearfix">
-		<div class="posts"><span class="white-bg text-uppercase red-text"><strong><?php _e($atts['title']);?></strong></span></div>				
+		<div class="posts"><span class="white-bg text-uppercase red-text"><strong><?php _e($atts['title']);?></strong></span></div>	
+		<div id="<?php _e( $atts['id'] );?>" data-target="<?php _e('#'.$atts['id'].' .row');?>" data-url="<?php _e( $atts['url'] );?>">	
 		<?php 
+			$i = 1;
 			while ( $this->query->have_posts() ) : $this->query->the_post(); 
 				$postid = $this->query->post->ID;
 				$image = '';
@@ -9,29 +11,44 @@
 					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $postid ), 'single-post-thumbnail' ); 				
 					$image = $image[0];
 				} 
+				
+				if( $i % 3 == 1){
+					echo "<div class='row'>";
+				}
+				
 		?>
-		<div class="col-sm-4">
-			<div class="card">
-				<div class="card-img" style="background-image:url(<?php echo $image?>);">
-					<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"></a>
-				</div>
-				<div class="card-block">
-					<h4 class="card-title red-text"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
-					<div class="card-text"><?php the_excerpt(); ?></div>
-					<div class="media">
-						<div class="media-left">
-							<a href="">
-							   <img class="media-object" src="<?php bloginfo('template_directory'); ?>/images/logo_small.png" alt="">
-							</a>
-						</div>
-						<div class="media-body small text-left">
-							<a href=""><strong class="media-heading red-text"><?php the_author();?></strong></a>
-							<div><?php echo get_the_date( 'M d' ); ?></div>
+		
+			<div class="col-sm-4 posts-query-article">
+				<div class="card">
+					<div class="card-img" style="background-image:url(<?php echo $image?>);">
+						<a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"></a>
+					</div>
+					<div class="card-block">
+						<h4 class="card-title red-text"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
+						<div class="card-text"><?php the_excerpt(); ?></div>
+						<div class="media">
+							<div class="media-left">
+								<a href="">
+								   <img class="media-object" src="<?php bloginfo('template_directory'); ?>/images/logo_small.png" alt="">
+								</a>
+							</div>
+							<div class="media-body small text-left">
+								<a href=""><strong class="media-heading red-text"><?php the_author();?></strong></a>
+								<div><?php echo get_the_date( 'M d' ); ?></div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		
+		<?php 
+				if( $i % 3 == 0 || $i == $this->query->post_count ){
+					echo "</div>";
+				}
+				$i++;
+			endwhile; 
+		?>
 		</div>
-		<?php endwhile; ?>
+		<?php the_pq_pagination( $atts );?>
     </section>
 </div>
