e0990522782bb99f1599ce368aabff8ecfbed4fb
Meenu Maria <meenu@sputznik.com>
2018-01-10 09:47:39 -0600

    template row, col added

--- template.php
+++ template.php
@@ -2,7 +2,11 @@
 <?php get_header();?>
 <?php while( have_posts() ) : the_post();?>
 	<div class="container space-above">
-		<?php the_content();?>
+		<div class="row">
+			<div class="col-md-12">
+				<?php the_content();?>
+			</div>
+		</div>
 	</div>
 <?php endwhile;?>
 <?php get_footer();?>
