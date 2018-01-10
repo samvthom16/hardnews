1d4be650a664895fa0eb24673bd90090d6f8da2c
Meenu Maria <meenu@sputznik.com>
2018-01-10 09:45:13 -0600

    new template

--- template.php
+++ template.php
@@ -1,6 +1,7 @@
+<?php /* Template Name: CustomPage */ ?>
 <?php get_header();?>
 <?php while( have_posts() ) : the_post();?>
-	<div class="container">
+	<div class="container space-above">
 		<?php the_content();?>
 	</div>
 <?php endwhile;?>
